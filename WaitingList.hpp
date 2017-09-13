d1c71ae2d8451a4c3bbe6e392d1c22cd9f31c4b2
proot <proot@CPUDebian>
2017-09-13 13:36:17 +0200

    test

---
+++ WaitingList.hpp
@@ -0,0 +1,14 @@
+#ifndef __WAITINGLIST_HPP
+#define __WAITINGLIST_HPP
+
+class WaitingList{
+    int longueurMax();
+    int longuerMoyenne();
+    double tempsMoyenAttente();
+    ajouter(Client);
+    bool estVide();
+    enlever();
+
+}
+
+#endif
